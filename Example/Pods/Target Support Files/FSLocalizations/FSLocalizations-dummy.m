ecf62cd1f1063b3e68961e667a5b44e931096d97
wanqijian <wanqijian@icloud.com>
2019-03-29 14:35:26 +0800

    first init

---
+++ Example/Pods/Target Support Files/FSLocalizations/FSLocalizations-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FSLocalizations : NSObject
+@end
+@implementation PodsDummy_FSLocalizations
+@end
---
+++ Example/Pods/Target Support Files/Pods-FSLocalizations_Tests/Pods-FSLocalizations_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FSLocalizations_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FSLocalizations_Tests
+@end
